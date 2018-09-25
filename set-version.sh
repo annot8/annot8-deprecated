98afc532331e2d3c415f8483505965028fd54a5f
Chris Flatley <chris@committed.io>
2018-09-25 15:10:50 +0100

    Add (untested) script to set version

---
+++ set-version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p "Enter version:" version
+
+mvn versions:set -DnewVersion=$version
+
+cd annot8-parent
+mvn versions:set -DnewVersion=$version
+cd -
+
+./git.sh commit -am "Bump to version $version"
+
+# The backup files are still around to delete or revert
+# mvn versions:commit
+# mvn versions:revert
+
