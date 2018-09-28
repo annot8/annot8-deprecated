958d7352a84834c20508ac4d48d53e9b817a3a0f
Chris Flatley <chris@committed.io>
2018-09-28 16:45:13 +0100

    Update README and scripts after testing

--- repository/setup-new-repo.sh
+++ repository/setup-new-repo.sh
@@ -9,14 +9,27 @@ fi
 
 echo "This script assumes that $1 is a public repo, cloned from GitHub"
 
-
-cp files/* $1/.
-cp scripts/* $1/.
-cp -r hooks $1/hooks
-
-cd travis
-./setup-travis $1
+cd $1 
+ABS_PATH=`pwd`
+git checkout -b new-repo-setup
+mkdir -p hooks
 cd -
 
-echo "cd $1; git commit -am 'Setup repository'"
+cp files/* $ABS_PATH/.
+cp scripts/* $ABS_PATH/.
+cp hooks/* $ABS_PATH/hooks/.
 
+cd travis
+./setup-travis.sh $ABS_PATH
+cd -
+
+cd $ABS_PATH
+git add setup.sh
+git add hooks/*
+git add LICENSE
+git add .travis.yml .travis.settings.xml
+git commit -am "Setup script complete"
+./setup.sh
+cd -
+
+echo "A commit has been made on your new repo, in the branch new-repo-setup. Check it before pushing to GitHub, and merging as a pull request".
--- repository/travis/setup-travis.sh
+++ repository/travis/setup-travis.sh
@@ -5,18 +5,19 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-if [if ! [ -x "$(command -v travis)" ]; then
+if ! [ -x "$(command -v travis)" ]; then
   echo 'Error: travis is not installed.' >&2
   echo 'Install with brew install travis'
   exit 1
 fi
 
-cp .travis.yml.template $1/.travis.yml
-cp .travis.settings.xml $1/.
+cp travis.yml.template $1/.travis.yml
+cp travis.settings.xml $1/.travis.settings.xml
 
+cd $1
 read -p "Enter Slack API Token: " SLACK_TOKEN
 travis encrypt $SLACK_TOKEN --add notifications.slack.rooms
 
 read -p "Enter SonarCloud API Token: " SONAR_TOKEN
 travis encrypt $SONAR_TOKEN --add addons.sonarcloud.token
-
+cd -
